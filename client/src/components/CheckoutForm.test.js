265e09fd19a08ebb85b563859de6a3facd12ff3b
elijahdaniel <e.paminsan@gmail.com>
2020-10-02 10:17:17 -0700

    all jest tests passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import CheckoutForm from './CheckoutForm'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test('form header renders', () => {
+  render(<CheckoutForm />)
+})
 
-test("form shows success message on submit with form details", () => {});
+test('form shows success message on submit with form details', () => {
+  render(<CheckoutForm />)
+
+  const btn = screen.getByRole('button')
+  btn.click()
+
+  const msg = screen.getAllByTestId('successMessage')
+  expect(msg).toBeTruthy()
+})
